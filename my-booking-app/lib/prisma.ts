9fe3d6adf681081d2e94623f42a4d7e5c2ac5b8f
Margaret Ajibola <margaretajibola56@yahoo.com>
2025-10-03 23:43:31 -0400

    database integration with prisma and postgresql plus added files for login, signup, and calendar pages

---
+++ my-booking-app/lib/prisma.ts
@@ -0,0 +1,15 @@
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // allow global singleton during dev to prevent many clients
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query"] : [],
+  });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
